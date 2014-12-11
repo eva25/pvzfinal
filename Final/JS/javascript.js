52ced1b9c6080fddcf262e515ca81a8ebeab58bb
eva25 <EVA@Evangelines-MacBook-Pro.local>
2014-12-10 22:27:32 -0800

    js

---
+++ Final/JS/javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("sequel").innerHTML =
+				"<ul id='menu'>" +
+					"<li><a href='sequel_pvz2.html' class='active'>Plants vs. Zombies 2: It's About Time</a></li>" +
+					"<li><a href='sequel_pvza.html'>PvZ Adventures</a></li>" +
+					"<li><a href='sequel_pvzgw.html'>PvZ Garden and Welfare</a></li>" +
+					"<li><a href='sequel_pvzo.html'>PvZ Online</a></li>" +
+				"</ul>";
---
+++ Final/JS/setup.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	
+	$('#pg').jphotogrid({
+		baseCSS: {
+			width: '175px',
+			height: '117px',
+			padding: '0px'
+		},
+		selectedCSS: {
+			position: 'relative',
+			top: '50px',
+			left: '50%',
+			width: '300px',
+			height: '250px',
+			padding: '10px'
+		}
+	});
+		
+});
