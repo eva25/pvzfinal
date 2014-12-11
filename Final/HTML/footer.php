8bed44c64a1a9f9fcef3eeda9c154bfaa188a351
eva25 <EVA@Evangelines-MacBook-Pro.local>
2014-12-10 22:25:15 -0800

    Html

---
+++ Final/HTML/footer.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<p>Copyright &copy; A00945106 . date("Y") . " Evangeline</p>";
+	echo '<a href="html/home.html">HOME</a> -
+	<a href="html/overview.html">PLANTS vs. ZOMBIES</a> -
+	<a href="html/sequels.html">SEQUELS</a> -
+	<a href="html/characters.html">CHARACTERS</a> -
+	<a href="html/strategies.html">STRATEGIES and TRIVIAS</a> -
+	<a href="html/techgal.html">TECH PAGE and GALLERY</a> -
+	<a href="html/blognews.html">BLOGS and NEWS</a>' ;
+?>
